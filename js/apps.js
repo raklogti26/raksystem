1324754b703f0b4fdd38671b977dc28027a00a01
raklogti26 <raklogti@gmail.com>
2026-05-21 15:00:58 -0300

    Update apps.js

--- js/apps.js
+++ js/apps.js
@@ -1,51 +1,70 @@
-
 function carregarTela(tela) {
 
     if (tela === "home") {
         document.getElementById("conteudo").innerHTML = `
+
+        <div class="titulo">PORTAL DA EMPRESA</div>
+
+        <div class="card-info">
+            <p><strong>Empresa:</strong> Sua Empresa Ltda</p>
+            <p><strong>CNPJ:</strong> 00.000.000/0001-00</p>
+            <p><strong>Responsável:</strong> TI Corporativo</p>
+        </div>
+
+        <div class="grid">
+
             <div class="card">
-                <h2>🏠 Dashboard</h2>
-                <p>Sistema interno da empresa</p>
+                <h4>Colaboradores</h4>
+                <div class="valor">10</div>
             </div>
+
+            <div class="card">
+                <h4>Equipamentos</h4>
+                <div class="valor">8</div>
+            </div>
+
+            <div class="card">
+                <h4>Ativos</h4>
+                <div class="valor">6</div>
+            </div>
+
+        </div>
         `;
     }
 
     if (tela === "colaboradores") {
         document.getElementById("conteudo").innerHTML = `
-            <div class="card">
-                <h2>👥 Colaboradores</h2>
+        <div class="titulo">Colaboradores</div>
 
-                <input placeholder="Nome"><br><br>
-                <input placeholder="Telefone"><br><br>
-
-                <button>Salvar</button>
-            </div>
+        <div class="card-info">
+            <input placeholder="Nome"><br><br>
+            <input placeholder="Telefone"><br><br>
+            <button>Salvar</button>
+        </div>
         `;
     }
 
     if (tela === "equipamentos") {
         document.getElementById("conteudo").innerHTML = `
-            <div class="card">
-                <h2>💻 Equipamentos</h2>
+        <div class="titulo">Equipamentos</div>
 
-                <input placeholder="Tipo"><br><br>
-                <input placeholder="Marca"><br><br>
-
-                <button>Salvar</button>
-            </div>
+        <div class="card-info">
+            <input placeholder="Tipo"><br><br>
+            <input placeholder="Marca"><br><br>
+            <button>Salvar</button>
+        </div>
         `;
     }
 
     if (tela === "admin") {
         document.getElementById("conteudo").innerHTML = `
-            <div class="card">
-                <h2>🔐 Admin</h2>
+        <div class="titulo">Administração</div>
 
-                <input placeholder="Email"><br><br>
-                <input placeholder="Senha"><br><br>
-
-                <button>Criar usuário</button>
-            </div>
+        <div class="card-info">
+            <input placeholder="Email"><br><br>
+            <input placeholder="Senha"><br><br>
+            <button>Criar usuário</button>
+        </div>
         `;
     }
 }
